08a2fe714fd9c391add4fdc91c18a4333329ee81
Wandana <dn.wandana@gmail.com>
2021-04-15 14:34:12 +0700

    include created and updated time to json response

--- server/models/url_model.go
+++ server/models/url_model.go
@@ -12,8 +12,8 @@ type Url struct {
 	ID        string             `json:"id" bson:"id"`
 	Title     string             `json:"title" bson:"title"`
 	URL       string             `json:"url" bson:"url"`
-	CreatedAt time.Time          `json:"-" bson:"createdAt"`
-	UpdatedAt time.Time          `json:"-" bson:"updatedAt"`
+	CreatedAt time.Time          `json:"createdAt" bson:"createdAt"`
+	UpdatedAt time.Time          `json:"updatedAt" bson:"updatedAt"`
 }
 
 type UrlResponse struct {
