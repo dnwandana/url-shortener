50f49661d62c34588fb5047097112536ee4c7145
Wandana <dn.wandana@gmail.com>
2021-04-28 19:57:55 +0700

    fix: inconsistent style

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -8,17 +8,17 @@ const Custom404Error = () => {
       <div className="min-h-screen px-4 flex flex-col justify-center items-center text-center bg-gray-100 space-y-5">
         <img
           className="w-1/2 md:w-1/4 lg:w-1/6"
-          src="/assets/svg/sorry.svg"
+          src="/assets/sorry.svg"
           alt="Sorry"
         />
-        <h1 className="text-gray-900 text-2xl md:text-3xl font-medium md:font-semibold">
+        <h1 className="text-gray-900 font-bold text-2xl md:text-3xl lg:text-4xl">
           Can't find what you looking for
         </h1>
-        <p className="text-gray-900 text-base md:text-lg font-normal">
+        <p className="text-gray-800 font-normal text-base md:text-lg lg:text-xl">
           I think you've clicked on a bad link or entered an invalid URL
         </p>
         <Link href="/">
-          <a className="py-2 px-4 rounded-md text-base font-medium bg-indigo-600 text-indigo-100 hover:bg-indigo-700 hover:text-indigo-50 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-opacity-50 transition-colors duration-300 ease-in-out">
+          <a className="py-2 px-4 rounded-md font-medium text-base text-center transition-colors duration-300 ease-in-out bg-indigo-700 text-indigo-100 hover:bg-indigo-600 hover:text-indigo-50 focus:outline-none focus:ring focus:ring-indigo-300 tracking">
             Back to Homepage
           </a>
         </Link>
