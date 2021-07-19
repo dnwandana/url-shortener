30d9b0cb8bf0acd7e1190bf4db79b7007a70e42b
Dana Wandana <dn.wandana@gmail.com>
2021-07-19 19:13:32 +0700

    add FindByID implementation

--- server/repository/user_repository.go
+++ server/repository/user_repository.go
@@ -2,12 +2,16 @@ package repository
 
 import (
 	"github.com/dnwandana/url-shortener/entity"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type UserRepository interface {
 	// Insert method is used to create a new users.
-	Insert(user *entity.User) (*entity.User, error)
+	Insert(user *entity.User) error
 
 	// FindByEmail method is used to get a specific user.
 	FindByEmail(email string) (*entity.User, error)
+
+	// FindByID method is used to get a specific user information.
+	FindByID(userID primitive.ObjectID) (*entity.User, error)
 }
--- server/repository/user_repository_impl.go
+++ server/repository/user_repository_impl.go
@@ -18,19 +18,31 @@ func NewUserRepository(collection *mongo.Collection) UserRepository {
 	}
 }
 
-func (r *UserRepositoryImpl) Insert(user *entity.User) (*entity.User, error) {
+func (r *UserRepositoryImpl) Insert(user *entity.User) error {
 	user.ID = primitive.NewObjectID()
 	_, err := r.Collection.InsertOne(context.Background(), user)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	return user, nil
+	return nil
 }
 
 func (r *UserRepositoryImpl) FindByEmail(email string) (*entity.User, error) {
 	var user *entity.User
 	filter := bson.M{
 		"email": email,
+	}
+	err := r.Collection.FindOne(context.Background(), filter).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (r *UserRepositoryImpl) FindByID(userID primitive.ObjectID) (*entity.User, error) {
+	var user *entity.User
+	filter := bson.M{
+		"_id": userID,
 	}
 	err := r.Collection.FindOne(context.Background(), filter).Decode(&user)
 	if err != nil {
