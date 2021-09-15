25a4b481d7dd3dbdc0780bd10eecb2b614dbc44e
Dana Wandana <dn.wandana@gmail.com>
2021-09-15 15:19:07 +0700

    entity/url: simplify URL struct

--- server/entity/url.go
+++ server/entity/url.go
@@ -2,15 +2,12 @@ package entity
 
 import (
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // URL struct represents the URL entity.
 type URL struct {
-	ObjectID  primitive.ObjectID `bson:"_id"`
-	ID        string             `bson:"id"`
-	URL       string             `bson:"url"`
-	SecretKey string             `bson:"secret_key"`
-	ExpireAt  time.Time          `bson:"expireAt"`
+	ID        string    `bson:"id"`
+	URL       string    `bson:"url"`
+	SecretKey string    `bson:"secret_key"`
+	ExpireAt  time.Time `bson:"expireAt"`
 }
